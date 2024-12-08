a53b90d56459e40041fc29c2a640957fc385076a
Camila-Isabela <camila_isabela_lins@hotmail.com>
2024-12-08 11:23:24 -0300

    add project management context menu to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,69 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
- 
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import WorkIcon from "@mui/icons-material/Work";
+
 function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  // Abrir o menu
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  // Fechar o menu
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Dashboard</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Projetos</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Tarefas</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Recursos</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Relatórios</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Configurações</MenuItem>
+          </Menu>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              flexGrow: 1,
+            }}
+          >
+            <WorkIcon sx={{ mr: 1 }} />
+            Gerenciamento de Projetos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
- 
+
 export default Header;
